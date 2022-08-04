2128801b91f18b3899bb6a39dfad59d29e4baf25
MagoroGraphics <magoro.graphics@gmail.com>
2022-08-04 16:39:03 +0100

    Styling gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,21 +1,44 @@
 import React from "react";
 import GalleryItem from "./GalleryItem";
+import styled from "styled-components";
 
-const Gallery = ({memes, userName}) => {
+const GalleryWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
 
-const allMemes = memes.map((meme, index) => {
-    return <GalleryItem key= {index} memeImg={meme.url} memeName={meme.name} />
-})
+const MainWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* justify-content: center; */
+  margin: auto;
+  background: #474bff;
+  background: -webkit-linear-gradient(0deg, #474bff 0%, #bc48ff 100%);
+  background: linear-gradient(0deg, #474bff 0%, #bc48ff 100%);
+  height: 100vh;
+  overflow: auto;
+`;
 
-    return (
-        <div>
-            <h1>Welcome to the Meme Gallery</h1>
-            <ul>
-                {allMemes}
-            </ul>
-        </div>
+const MemeItemsWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
 
-    )
-}
+const Gallery = ({ memes, userName }) => {
+  const allMemes = memes.map((meme, index) => {
+    return <GalleryItem key={index} memeImg={meme.url} memeName={meme.name} />;
+  });
 
-export default Gallery
+  return (
+    <MainWrapper>
+      <h1>Welcome to the Meme Gallery</h1>
+      <MemeItemsWrapper>
+        <GalleryWrapper>{allMemes}</GalleryWrapper>
+      </MemeItemsWrapper>
+    </MainWrapper>
+  );
+};
+
+export default Gallery;
--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,30 +1,46 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 
-const GalleryItem = ({memeImg, memeName}) => {
-    
-    const GalleryListItem = styled.li`
-        display: flex;
-        flex-direction: column;
-        text-align: center;
-        
-    `
+const GalleryItem = ({ memeImg, memeName }) => {
+  const GalleryListItem = styled.li`
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    padding: 1vh;
+    -webkit-box-shadow: 3px 3px 10px 3px #ddd;
+    -moz-box-shadow: 3px 3px 10px 3px #ddd;
+    box-shadow: 3px 3px 10px 3px #ddd;
+    width: 30vw;
+    height:32vh;
+    margin: 2vh;
+    border-radius: 2vh;
+    background-color: #ffb6e4;
+    font-weight: bold;
+  `;
 
-    const GalleryMemeImg = styled.img`
-        max-width: 500px;
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-        width: 50%;
-        
-    `
-    
-    return (
-        <GalleryListItem>
-            <GalleryMemeImg src={memeImg}/>
-            {memeName}
-        </GalleryListItem>
-    )
-}
+  const GalleryMemeImg = styled.img`
+    max-width: 500px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+    border-radius: 1vh;
+  `;
 
-export default GalleryItem
+  const MainWrapper = styled.div`
+    display: flex;
+    margin:2vh;
+    justify-content: center;
+  `;
+
+  return (
+    <MainWrapper>
+      <GalleryListItem>
+        <GalleryMemeImg src={memeImg} />
+        {memeName}
+      </GalleryListItem>
+    </MainWrapper>
+  );
+};
+
+export default GalleryItem;
